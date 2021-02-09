2fe82eda0da2f1c623caa4f1d467e70a69167fa9
malangwane <wethuwiseman@gmail.com>
2021-02-09 19:30:54 +0200

    second commit

--- app/Customer.ts
+++ app/Customer.ts
@@ -11,14 +11,29 @@ export class Customer implements User{
     locked: boolean;
     failedLoginAttempts: number;
     LastLoingDate: Date;
+    constructor(fname: string, sname: string, nname: string | undefined,  
+        email: string, r: string,  created: Date, Removed: Date | undefined, islocked: boolean,failedLogin: number, LastLoing: Date,){
+            this.forename = fname;
+            this.surname =sname;
+            this.nickname = nname;
+            this.emailAddress = email;
+            this.role = r;
+            this.createdDate = created;
+            this.dateRemoved =Removed;
+            this.locked =islocked;
+            this.failedLoginAttempts = failedLogin;
+            this.LastLoingDate =LastLoing;
 
-
+    }
     FirstName(): string {
         if(this.nickname == undefined){
-            return this.forename.substring(this.forename.indexOf(this.forename),
+            let forname = this.forename.substring(this.forename.indexOf(this.forename),
                                             this.forename.indexOf(' ',this.forename.indexOf(this.forename))- this.forename.indexOf(this.forename));
+                                            return forname;
         }
+        return this.nickname;
     }
+
 
     FullName(): string {
         return this.FirstName() + this.forename;
--- app/SalesRep.ts
+++ app/SalesRep.ts
@@ -13,6 +13,23 @@ class SalesRep implements User{
     failedLoginAttempts: number;
     customers: Customer[];
 
+    constructor(fname: string, sname: string,nname: string | undefined,email: string,r: string,created: Date,removed: Date | undefined,
+        islocked: boolean,failedLoginAttempts: number,){
+
+            this.forename = fname;
+            this.surname = sname;
+            this.nickname =nname;
+            this.emailAddress = email;
+            this.role = r;
+            this.createdDate = created;
+            this.dateRemoved = removed;
+            this.locked =islocked;
+            this.failedLoginAttempts = failedLoginAttempts;
+            this.customers = [];
+
+    }
+
+
     FirstName(): string {
         return this.forename.substring(this.forename.indexOf(this.forename),
         this.forename.indexOf(' ',this.forename.indexOf(this.forename))- this.forename.indexOf(this.forename));
