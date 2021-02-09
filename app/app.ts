8061881ad42f028efc75d24a8b5a76809f7f998e
malangwane <wethuwiseman@gmail.com>
2021-02-09 19:35:16 +0200

    3rd

--- app/app.ts
+++
@@ -1,6 +0,0 @@
-function startGame(){
-    var messageElement = document.getElementById('message');
-    messageElement!.innerText = 'welcome to the game';
-}
-
-document.getElementById('startGame')?.addEventListener('click',startGame);
